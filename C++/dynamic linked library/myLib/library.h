2c1e65e482418f22e3716c2d7b765e161391a068
liangfeng1234 <101499419+liangfeng1234@users.noreply.github.com>
2023-05-27 17:10:24 +0800

    C++动态链接库测试

---
+++ C++/dynamic linked library/myLib/library.h
@@ -0,0 +1,6 @@
+#ifndef MYLIB_LIBRARY_H
+#define MYLIB_LIBRARY_H
+
+void hello(void);
+
+#endif //MYLIB_LIBRARY_H
---
+++ C++/dynamic linked library/testMyLib/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+void hello();
+
+int main() {
+    hello();
+    return 0;
+}
